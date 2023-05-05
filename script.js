9f2f2702944af6a585cabfed24c545f1cf22ecf4
Shaik Hafeez Basha <115686162+ShaikHafeezBasha@users.noreply.github.com>
2023-05-05 17:04:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function volume_sphere() {
-    //Write your code here
-  
+    let rad=document.getElementById('radius')
+	let vol=document.getElementById('volume') 
+	let r=rad.value
+	let v=(4*22*r*r*r)/21
+	vol.value=v
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
